d9c3d40be9f33f01ce7a0803b2ef0f033fcfcbb0
Brayan-Fon <brayanstevenfontechalozano@gmail.com>
2025-08-24 21:49:04 -0500

    Delete Reto2.py

--- Reto2.py
+++
@@ -1,9 +0,0 @@
-class Dog:
-    name = input("Digite el nombre de su perro " ) 
-    objeto = input("Que trae el perro?") 
-    
-    def fetch (self):
-        print(f"{self.name}, trajo {self.objeto}")
-
-name = Dog() 
-name.fetch()
